7f1362b8b937565c13f643bc93e2deff88213cfb
Praise Fakorede <praise8fakorede@gmail.com>
2022-09-16 13:32:01 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 
-/** 
+/**
  * main - Printing
  *
  * Return: Always 0 (Succesful)
  */
 int main(void)
 {
-	puts("\" Programming is like building a multilingual puzzle");
+	puts("\ "Programming is like building a multilingual puzzle");
 	return(0);
 }
 
