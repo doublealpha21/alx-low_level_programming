a8f4147ba55fa52471f0517775da2825026eb254
Praise Fakorede <praise8fakorede@gmail.com>
2023-08-21 01:58:32 -0700

    Update 503.py

--- 503.py
+++ 503.py
@@ -1,3 +1,22 @@
+from scipy.optimize import linprog
+
+# Coefficients of the objective function
+c = [-2, -3]
+
+# Coefficients of the inequality constraints
+A = [[2, 1], [2, 3]]
+
+# Right-hand side values of the inequality constraints
+b = [140, 180]
+
+# Bounds for the decision variables x and y
+x_bounds = (0, None)
+y_bounds = (0, None)
+
+# Solve the optimization problem
+result = linprog(c, A_ub=A, b_ub=b, bounds=[x_bounds, y_bounds], method='highs')
+
+# Display the results
 if result.success:
     x_optimal = result.x[0]
     y_optimal = result.x[1]
